977cda3890156d137ee52d3d4a158d6c0752bfa6
marcosvors <marcosvors2022@gmail.com>
2026-04-17 14:30:29 -0300

    Category

---
+++ src/main/java/com/educaweb/course/repositories/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.educaweb.course.repositories;
+
+import com.educaweb.course.entities.Category;
+import com.educaweb.course.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+}
